a33fa1631c23b005712e5559729aafd2d3581303
Raghuveer N <narahar2@uwm.edu>
2023-01-04 18:43:05 -0600

    small fixes

--- src/main/scala/lyra/Main.scala
+++ src/main/scala/lyra/Main.scala
@@ -7,7 +7,6 @@ import org.scalajs.dom
 def Lyra(): Unit = {
     val canvasElement = dom.document.querySelector("#appCanvas").asInstanceOf[dom.HTMLCanvasElement]
     val app = App(canvasElement, List[Shape]())
-    println("after app")
 //    val ctx = canvasElement.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
 //    ctx.fillStyle = "black"
 //    ctx.fillRect(0,0,255,255.0)
